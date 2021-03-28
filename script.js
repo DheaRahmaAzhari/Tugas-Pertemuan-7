6b0d360b842fa97e4bd29c6486dfcb96f7995493
DheaRahmaAzhari <79326691+DheaRahmaAzhari@users.noreply.github.com>
2021-03-28 10:23:14 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-var nilaiawal = 1;
-while (nilaiawal <= 10) {
-	console.log ('hello world ' + nilaiawal + 'x');
-	nilaiawal++;
+function tambah() {
+	var hasil = 0;
+	for ( var i = 0; i < arguments.length; i++) {
+		hasil += arguments[i];
+	}
+	return hasil;
 }
+
+var coba = tambah(1,2,3,4,5);
+console.log(coba);
